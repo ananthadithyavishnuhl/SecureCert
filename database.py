c5d349d39414146c22b79b3ad9391e2edf1a9aaf
ananthadithyavishnuhl <ananthadithya.vishnuhl@gmail.com>
2026-03-13 07:27:53 +0530

    Database.py updated

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ name text NOT NULL,
 designation text NOT NULL,
 course text NOT NULL,
 year INTEGER NOT NULL,
-expiry_date text NOT NULL,
+expiry_date text,
 hash text NOT NULL)""")
 conn.commit()
 conn.close()
@@ -27,27 +27,40 @@ def insert_certificate(certificate):
             certificate["expiry"],
             certificate["hash"]))
     conn.commit()
+    conn.close()
     merkleRoot()
 
 def get_certificate(credential_id):
+    conn = sqlite3.connect('Certificates.db')
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Certificates WHERE certificateID = ?",
                    (credential_id,))
     cert = cursor.fetchone()
+    conn.close()
     return cert
 def get_certificate_hash():
+    conn = sqlite3.connect('Certificates.db')
+    cursor = conn.cursor()
     cursor.execute("SELECT hash FROM Certificates")
     hash = cursor.fetchall()
+    conn.close()
     return [r[0] for r in hash]
 def delete_certificate(credential_id):
+    conn = sqlite3.connect('Certificates.db')
+    cursor = conn.cursor()
     cursor.execute("DELETE FROM Certificates WHERE certificateID = ?",
                    (credential_id,))
     conn.commit()
+    conn.close()
     merkleRoot()
 def merkleRoot():
     global current_root
+    conn = sqlite3.connect('Certificates.db')
+    cursor = conn.cursor()
     cursor.execute("SELECT hash FROM Certificates")
     Data = cursor.fetchall()
     hashes=[r[0] for r in Data]
     current_root=merkle_tree_create(hashes)
+
 
 
